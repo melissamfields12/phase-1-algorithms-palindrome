c63beb8dd4bd31c68e63a8b0052aac4ea04df96f
Melissa Fields <melissamfields12@gmail.com>
2022-10-27 16:12:30 -0700

    commit tree de273e6e1ce9cb5ceb9f75ac62fb826e5dea6430

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const wordLength = word.length;
+  for (let i = 0; i < wordLength/2; i++) {
+    if(word[i] !== word[wordLength -1 -i]){
+      return false
+    }
+  }
+  return true
 }
 
 /* 
   Add your pseudocode here
+  First find the word length. 
+  Next use a method to iterate over the letters in the word.
+  Find method to split the word.
+  Use method to compare each half of the word. 
 */
 
 /*
   Add written explanation of your solution here
+  First find the word length. Next iterate over the letters in the word using a for loop,
+  while including wordLength divided by two to split the word. Finally, use an if statement
+  to verify the first and last characters are the same and to determine if each half of the 
+  word is the same or not.
 */
 
 // You can run `node index.js` to view these console logs
